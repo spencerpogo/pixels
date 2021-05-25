2740ae361c71db68cef9745f292d5d7bc81232a1
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-05-25 14:29:36 -0700

    Remove p5 app

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from os import getenv
-from typing import Callable
-
 from aiohttp import web, ClientSession
 
 app = web.Application()
 routes = web.RouteTableDef()
 
-IS_PROD = getenv("PYTHON_ENV", "").lower().startswith("prod")
 API_URL = "https://pixels.pythondiscord.com"
 
 
@@ -16,29 +12,6 @@ async def index(req: web.Request) -> web.Response:
     return web.FileResponse("index.html")
 
 
-@routes.view("/api/{endpoint}")
-async def api_proxy(req: web.Request) -> web.Response:
-    async with ClientSession() as sess:
-        url = API_URL + "/" + req.match_info["endpoint"]
-        print(req.method, url)
-        reqheaders = req.headers.copy()  # make mutable
-        del reqheaders["host"]
-        reqheaders["user-agent"] = "pixels/0.1.0; https://github.com/Scoder12/pixels"
-        async with sess.request(
-            req.method,
-            url,
-            headers=reqheaders,
-            params=req.rel_url.query,
-            data=await req.read(),
-        ) as res:
-            resheaders = res.headers.copy()  # make mutable
-            resheaders["access-control-allow-origin"] = "*"
-            return web.Response(
-                body=await res.read(), status=res.status, headers=resheaders
-            )
-
-
-app.add_routes([web.static("/src", "src")])
 app.add_routes(routes)
 
 
